7cfbfadc8529db94a85a8e444ce7521c7e27e5e9
Orffen <orffen@orffenspace.com>
2021-02-19 12:02:40 +1000

    Only allow a single instance of the program to run

--- Ship's Bells/Program.cs
+++ Ship's Bells/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Ships_Bells
@@ -11,12 +9,32 @@ namespace Ships_Bells
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        static Mutex mutexShipsBells;
+
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            if (Program.IsSingleInstance())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainWindow());
+            }
+        }
+
+        static bool IsSingleInstance()
+        {
+            try
+            {
+                Mutex.OpenExisting("mutexShipsBells");
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                Program.mutexShipsBells = new Mutex(true, "mutexShipsBells");
+                return true;
+            }
+            return false;
         }
     }
 }
